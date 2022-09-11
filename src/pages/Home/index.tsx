9d2e0f8efe5159b84b08cb58c2d1f861ab5594cb
Madiou BAH <mmadioubah@gmail.com>
2022-09-11 21:13:37 +0200

    Navigation added

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import Navigation from './components/Navigation'
+
+export default function Home() {
+    return (
+        <div className="min-w-full min-h-full">
+            <Navigation/>
+        </div>
+    );
+}
